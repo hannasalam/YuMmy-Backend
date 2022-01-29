7770900ce1850a6cc0db9a550eb09b9664ee8421
hannasalam <hannasalam113@gmail.com>
2022-01-29 21:29:17 +0530

    added headers

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,6 +22,15 @@ module.exports = function(app){
         
 
     app.post("/register", async(req,res)=>{
+        res.setHeader('Access-Control-Allow-Credentials', true)
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        // another common pattern
+        // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+        res.setHeader(
+          'Access-Control-Allow-Headers',
+          'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+        )
         try{
         const user = new User({
             name: req.body.name,
