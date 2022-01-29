fe125d0d4ed3e39f3434d9cb21ff094469b3953b
Hanna Salam <74866650+hannasalam@users.noreply.github.com>
2022-01-29 22:09:48 +0530

    added headers

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -3,6 +3,15 @@ const Cart = require('../models/Cart');
 module.exports = function(app){
 
     app.post("/addToCart", async (req, res) => {
+        res.setHeader('Access-Control-Allow-Credentials', true)
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        // another common pattern
+        // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+        res.setHeader(
+          'Access-Control-Allow-Headers',
+          'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+        )
         const newCart = new Cart(req.body);
 
         try {
@@ -15,6 +24,15 @@ module.exports = function(app){
         
 
     app.get("/getCart/:userId", async(req,res)=>{
+        res.setHeader('Access-Control-Allow-Credentials', true)
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        // another common pattern
+        // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+        res.setHeader(
+          'Access-Control-Allow-Headers',
+          'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+        )
         try {
             const cart = await Cart.find({ userId: req.params.userId });
             res.status(200).json(cart);
--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,15 @@ const User = require('../models/User');
 module.exports = function(app){
  
   app.post("/login", async (req, res) => {
+        res.setHeader('Access-Control-Allow-Credentials', true)
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        // another common pattern
+        // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+        res.setHeader(
+          'Access-Control-Allow-Headers',
+          'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+        )
         const { name, password } = req.body;
         let user = await User.findOne({ name });
         console.log(user);
